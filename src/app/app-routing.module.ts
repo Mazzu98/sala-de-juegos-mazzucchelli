f812581f9a1f389ed27da1f0303c406f43f96b24
Mazzu98 <juani.mp1@gmail.com>
2021-08-29 21:51:48 -0300

    login

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,9 +5,9 @@ import { LoginComponent } from './components/login/login.component';
 import { QuienSoyComponent } from './components/quien-soy/quien-soy.component';
 
 const routes: Routes = [
-  {path: '', redirectTo: '/home',pathMatch: 'full'},
-  {path: 'home', component: HomeComponent},
+  {path: '', redirectTo: '/login',pathMatch: 'full'},
   {path: 'login', component: LoginComponent},
+  {path: 'home', component: HomeComponent},
   {path: 'quien-soy', component: QuienSoyComponent},
 ];
 
