6127f35a77d261dedea3fc62c0044cd56620c1fd
Mazzu98 <juani.mp1@gmail.com>
2021-08-30 12:34:15 -0300

    nav

--- src/app/components/nav/nav.component.ts
+++ src/app/components/nav/nav.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-nav',
@@ -9,8 +10,8 @@ export class NavComponent implements OnInit {
 
   loggedIn: boolean;
 
-  constructor() {
-    this.loggedIn = false;
+  constructor(public router: Router) {
+    this.loggedIn = true;
   }
 
   ngOnInit() {
