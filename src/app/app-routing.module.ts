2bcd44e0d2f4d8f877306749363dc4f37c5961ae
Mazzu98 <juani.mp1@gmail.com>
2021-09-08 18:28:37 -0300

    sin guard

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -12,7 +12,7 @@ const routes: Routes = [
   {path: '', redirectTo: '/home',pathMatch: 'full'},
   {path: 'login', component: LoginComponent},
   {path: 'register', component: RegisterComponent},
-  {path: 'home', component: HomeComponent, canActivate: [AuthGuard]},
+  {path: 'home', component: HomeComponent,},
   {path: 'quien-soy', component: QuienSoyComponent},
   {path: '**', component: ErrorComponent}
 ];
