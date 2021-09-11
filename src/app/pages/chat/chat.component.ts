b453939b5097efbcf863e1f460c467ba98998ff6
Mazzu98 <juani.mp1@gmail.com>
2021-09-11 02:50:54 -0300

    chat piola

--- src/app/pages/chat/chat.component.ts
+++ src/app/pages/chat/chat.component.ts
@@ -1,23 +1,27 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterViewChecked, Component, OnInit, ViewChild } from '@angular/core';
 import { AuthService } from 'src/app/services/auth.service';
 import { ChatService } from 'src/app/services/chat.service';
+import { Renderer2 } from '@angular/core';
+import { ElementRef } from '@angular/core';
 
 @Component({
   selector: 'app-chat',
   templateUrl: './chat.component.html',
   styleUrls: ['./chat.component.css']
 })
-export class ChatComponent implements OnInit {
+export class ChatComponent implements OnInit, AfterViewChecked{
+
+  @ViewChild("chat") chat: ElementRef;
 
   messages:any;
   message: any;
 
-  constructor(private chatS: ChatService, public auth:AuthService) { 
+  constructor(private chatS: ChatService, public auth:AuthService, private render: Renderer2) { 
     this.messages = chatS.items;
-    console.log((new Date()).getMinutes());
   }
 
   send(){
+    this.chat.nativeElement.scrollTop = -100;
     let now = new Date();
     let timeNow = now.getHours() + ":" + now.getMinutes();
     let message = {
@@ -28,6 +32,16 @@ export class ChatComponent implements OnInit {
     this.chatS.sendMessage(message).then(()=>{
       this.message = "";
     });
+  }
+
+  ngAfterViewChecked() {        
+    this.scrollToBottom();        
+  } 
+
+  scrollToBottom(): void {
+    try {
+        this.chat.nativeElement.scrollTop = this.chat.nativeElement.scrollHeight;
+    } catch(err) { }                 
   }
 
   ngOnInit(): void {
